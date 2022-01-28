68f3ba95ae2139416ef37da9d472679a6d36b307
bmadina <bmadina@birt.eus>
2022-01-28 21:58:50 +0100

    added forms

--- src/main/java/eus/birt/dam/controller/StudentController.java
+++ src/main/java/eus/birt/dam/controller/StudentController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import eus.birt.dam.domain.Student;
 import eus.birt.dam.repository.StudentRepository;
-
 
 @Controller
 public class StudentController {
@@ -19,5 +22,30 @@ public class StudentController {
 		model.addAttribute("students",studentRepository.findAll());
 		return "students/list";
 	}
-
+	
+	@GetMapping("/students/new")
+	public String initCreationForm(Model model) {
+		Student student = new Student();
+		model.addAttribute("student",student);
+		return "students/studentForm";
+	}
+	
+	@PostMapping ("students/new/submit")
+	public String submitCreationForm(@ModelAttribute Student student) {
+		studentRepository.save(student);
+		return "redirect:/students";
+	}
+	
+	@GetMapping ("students/edit/{id}")
+	public String initEditForm(@PathVariable("id")int id, Model model) {
+		model.addAttribute("student", studentRepository.findById(id));
+		return "students/studentForm";
+	}
+	
+	@GetMapping ("students/delete/{id}")
+	public String initDelete(@PathVariable ("id") int id) {
+		studentRepository.deleteById(id);
+		return "redirect:/students";
+		
+	}
 }
